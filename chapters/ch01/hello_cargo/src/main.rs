91ee72d4b31eee3a1c2ff4ebf493bf3fad6c53a8
ianflexa <ianflexa@gmail.com>
2023-12-26 11:12:37 -0400

    chapter 1

---
+++ chapters/ch01/hello_cargo/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+}
+//                  *** notes ***
+/*
+    - rust filanme convention is snake_case eg: learning_rust.rs
+    - main function is always the first code that runs
+    - we can format our code by doing: rustfmt <file.rs>
+    - println! with the "!" calls a rust macro (more in chapter 19).
+    - "!" means calling a macro
+    - compiling: rustc <file.rs>
+    - we can use `cargo build` to build the entering project, and cargo run
+    - `cargo check` checks if the code will compile (faster than `cargo build`)
+
+*/
