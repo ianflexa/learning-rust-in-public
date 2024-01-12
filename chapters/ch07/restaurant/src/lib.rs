1b1c63cd1abe7f954962c3210472f994c5addfb8
ianflexa <ianflexa@gmail.com>
2024-01-12 12:26:34 -0400

    ch07: grouping related  code in modules

---
+++ chapters/ch07/restaurant/src/lib.rs
@@ -0,0 +1,36 @@
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+
+// ***** notes *****
+
+/*
+    - Modules let us organize code within a crate for readability and easy reuse.
+    -lso allow us to control the privacy of items, because code within a module is private by default.
+
+*/
